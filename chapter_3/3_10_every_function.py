b19ff00f86bfc99810e1cf3013742228bcd53b57
Carlos M Garcia <cgarcia101015@gmail.com>
2020-04-11 22:54:43 -0400

    updating MacBook

---
+++ chapter_3/3_10_every_function.py
@@ -0,0 +1,11 @@
+family_members = ['cynthia', 'lucas', 'papi', 'mami', 'ralph', 'brian', 'jennifer']
+
+print(f"sorted list {sorted(family_members)}")
+
+print(f"sorted list in reverse {sorted(family_members, reverse = True)}")
+
+family_members.reverse()
+print(f"reveresed list {family_members}")
+
+family_members.sort()
+print(f"permanently sorted list {family_members}")
---
+++ chapter_3/3_9_dinner_guests.py
@@ -0,0 +1,6 @@
+guest_list = ['Anthony Bourdain', 'Dave Chapplle', 'Joe Rogan']
+print(f"Hi {guest_list[0]} would you like to join us for dinner?")
+print(f"Hi {guest_list[1]} would you like to join us for dinner?")
+print(f"Hi {guest_list[2]} would you like to join us for dinner?")
+
+print(f"Number of people invited: {len(guest_list)}")
