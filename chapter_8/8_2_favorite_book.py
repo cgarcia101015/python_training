024e5589cbd4905ced48527f0e5d94c9a1453661
Carlos M Garcia <cgarcia101015@gmail.com>
2020-05-02 00:16:36 -0400

    starting functions

---
+++ chapter_8/8_2_favorite_book.py
@@ -0,0 +1,14 @@
+# def favorite_book(title):
+# 	print("One of my favorite books is {}".format(title.title()))
+
+# favorite_book('candide')
+
+# Passing arguments
+# Positional Arguments
+
+def describe_pet(animal_type, pet_name):
+	"""Display information about a pet."""
+	print(f"\nI have a {animal_type}.")
+	print(f"My {animal_type}'s name is {pet_name.title()}.")
+
+describe_pet('tiger', 'tony')
